39ec58161efeee68f579a4a2b76240713481d1b6
Wilson Zarantonello Junior <wilson.zarantonello@outlook.com>
2020-09-11 09:13:25 -0300

    Ajustes

--- LabNotasTests/NotasFrequencias.cs
+++ LabNotasTests/NotasFrequencias.cs
@@ -6,12 +6,27 @@ namespace LabNotasTests
     public class NotasFrequencias
     {
         private const string aprovado = "Aprovado";
+        private const string reprovado = "Reprovado";
 
         [TestMethod]
         public void AprovacaoDisciplina()
         {
-            string resultado = LabNotas.Program.ValidaNotaFrequencia(5, 0.7);
+            string resultado = LabNotas.Program.ValidaNotaFrequencia(8, 0.7);
             Assert.AreEqual(resultado, aprovado);
+        }
+
+        [TestMethod]
+        public void AprovacaoMinimaDisciplina()
+        {
+            string resultado = LabNotas.Program.ValidaNotaFrequencia(5, 0.6);
+            Assert.AreEqual(resultado, aprovado);
+        }
+
+        [TestMethod]
+        public void ReprovacaoDisciplina()
+        {
+            string resultado = LabNotas.Program.ValidaNotaFrequencia(4, 0.7);
+            Assert.AreEqual(resultado, reprovado);
         }
     }
 }
